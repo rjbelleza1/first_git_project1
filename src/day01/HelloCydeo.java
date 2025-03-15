a030e776486b5b0e6e32b681e936d09c8b21feaa
rjbelleza1 <ralphbelleza@yahoo.com>
2025-03-15 14:13:31 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -5,5 +5,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo!");
+        System.out.println("Hello Cydeo!");
+        System.out.println("Hello Cydeo!");
+
+        // Some changes are added.
     }
 }
